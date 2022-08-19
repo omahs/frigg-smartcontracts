aacfeaecfe3347d4c2498f6d5118326da4f6b4e1
owieth <olivier.winkler@icloud.com>
2022-08-19 18:54:46 +0200

    feat(testing): add ATT unit test about expiry

--- test/unit/ATT.ts
+++ test/unit/ATT.ts
@@ -4,7 +4,8 @@ import { loadFixture } from 'ethereum-waffle';
 
 describe("ATT", function () {
   async function getContracts() {
-    const MULTISIG = "0xBA072c1018cec4a1A8A6abF2971a37549e3B5aCF";
+    const [owner, addr1] = await ethers.getSigners();
+    const MULTISIG = addr1.address;
 
     const ATT = await ethers.getContractFactory("ATT");
     const PRIMARY_ROUTER = await ethers.getContractFactory("primaryRouter");
@@ -13,7 +14,7 @@ describe("ATT", function () {
     console.log("deployed router: ", primaryRouter.address);
 
     const att = await ATT.deploy(MULTISIG, primaryRouter.address);
-    return { att, primaryRouter };
+    return { att, primaryRouter, owner, addr1 };
   }
 
     it("Check if primary market is active", async function () {
@@ -27,9 +28,15 @@ describe("ATT", function () {
     });
 
     it("Should revert because caller is not the admin", async function () {
-      const { att } = await loadFixture(getContracts);
-      const [owner] = await ethers.getSigners();
+      const { att, owner } = await loadFixture(getContracts);
 
-      await expect(att.setBondExpiry()).to.be.revertedWith(`AccessControl: account ${owner.address} is missing role 0x0000000000000000000000000000000000000000000000000000000000000000`);
+      expect(await att.setBondExpiry()).to.be.revertedWith(`AccessControl: account 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000`);
+    });
+
+    it("Should set bond expiry to true", async function () {
+      const { att, addr1 } = await loadFixture(getContracts);
+
+      expect(await att.connect(addr1).setBondExpiry());
+      expect(await att.seeBondExpiryStatus()).to.equal(true)
     });
 });
