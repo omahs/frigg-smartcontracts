8cb9750ba8563449e956151e3f2bfd5c36683de0
owieth <olivier.winkler@icloud.com>
2022-08-26 11:03:06 +0200

    feat(contracts): format with hardhat + prettier

--- contracts/Token/IFrigg.sol
+++ contracts/Token/IFrigg.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.0;
 
 /*
-* This interface is specific for Frigg-implemented tokens 
-*/
+ * This interface is specific for Frigg-implemented tokens
+ */
 
 interface IFrigg {
     /**
-    * @dev returns if primary market is opened.
-    */
+     * @dev returns if primary market is opened.
+     */
     function isPrimaryMarketActive() external view returns (bool);
 
     /**
-    * @dev returns if the bond has expired and the issuer starts to conduct buyback.
-    */
+     * @dev returns if the bond has expired and the issuer starts to conduct buyback.
+     */
     function seeBondExpiryStatus() external view returns (bool);
 
     /**
@@ -29,7 +29,7 @@ interface IFrigg {
     function burn(address account, uint256 amount) external;
 
     /**
-     * @dev a getter function for dApps or third parties to fetch the terms and conditions 
+     * @dev a getter function for dApps or third parties to fetch the terms and conditions
      */
     function termsURL() external view returns (string memory);
 }
