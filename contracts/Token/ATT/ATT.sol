add80d06414554f019cd9579289b3690243c0c30
owieth <olivier.winkler@icloud.com>
2022-08-26 13:11:19 +0200

    feat(contracts): add natspec comments & uid modifier

--- contracts/Token/ATT/ATT.sol
+++ contracts/Token/ATT/ATT.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
@@ -9,25 +10,28 @@ import "../IFrigg.sol";
 /// @author Frigg team
 /// @dev inherits the OpenZepplin ERC20Capped, AccessControl and Frigg token standard implementations
 contract ATT is ERC20Capped, AccessControl, IFrigg {
+    using SafeERC20 for ERC20;
+
     /// @dev ROUTER_ROLE has permission to mint and burn tokens
     bytes32 public constant ROUTER_ROLE = keccak256("ROUTER_ROLE");
 
-    using SafeERC20 for ERC20;
+    /// @dev The default value is false
+    bool public isBondExpired;
 
-    //cap the supply of token to 20,000
+    /// @notice cap the supply of token to 20,000
     constructor(address _multisig, address _router)
         ERC20("Agatobwe", "ATT")
         ERC20Capped(20000 * (10**18))
     {
-        //set DEFAULT_ADMIN_ROLE to a multisig address controlled by Frigg
-        //DEFAULT_ADMIN_ROLE is already implemented in AccessControl contract
+        /// Set DEFAULT_ADMIN_ROLE to a multisig address controlled by Frigg
+        /// DEFAULT_ADMIN_ROLE is already implemented in AccessControl contract
         _grantRole(DEFAULT_ADMIN_ROLE, _multisig);
 
-        //set MINTER_ROLE to router
+        /// Set MINTER_ROLE to router
         _grantRole(ROUTER_ROLE, _router);
     }
 
-    /// @dev this function only allows router to mint new tokens during primary market sale
+    /// @dev This function only allows router to mint new tokens during primary market sale
     function mint(address _to, uint256 _amount)
         public
         override
@@ -36,7 +40,7 @@ contract ATT is ERC20Capped, AccessControl, IFrigg {
         _mint(_to, _amount);
     }
 
-    /// @dev this function only allows router to burn existing tokens when tokens reach expiry
+    /// @dev This function only allows router to burn existing tokens when tokens reach expiry
     function burn(address _from, uint256 _amount)
         public
         override
@@ -45,25 +49,22 @@ contract ATT is ERC20Capped, AccessControl, IFrigg {
         _burn(_from, _amount);
     }
 
-    /// @dev as totalSupply = cap, this function returns false
+    /// @dev If totalSupply = cap, this function returns false
     function isPrimaryMarketActive() public view override returns (bool) {
         return totalSupply() < cap();
     }
 
-    /// @dev the default value is false
-    bool public isBondExpired;
-
-    /// @dev deployer of this contract can modify the value of isBondExpired
-    function setBondExpiry() public onlyRole(DEFAULT_ADMIN_ROLE) {
-        isBondExpired = true;
-    }
-
-    /// @dev getter function for router contract to access variable
+    /// @dev Getter function for router contract to access variable
     function seeBondExpiryStatus() public view override returns (bool) {
         return isBondExpired;
     }
 
-    //Terms will contain the terms of issue for this particular token
-    //To be updated before mainnet deployment
+    /// @notice Terms will contain the terms of issue for this particular token
+    /// @dev To be updated before mainnet deployment
     string public override termsURL = "https://www.agatobwe.eco/terms-of-issue";
+
+    /// @dev Deployer of this contract can modify the value of isBondExpired
+    function setBondExpiry() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        isBondExpired = true;
+    }
 }
--- contracts/Token/IFrigg.sol
+++ contracts/Token/IFrigg.sol
@@ -1,35 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/*
- * This interface is specific for Frigg-implemented tokens
- */
-
+/// @title This interface is specific for Frigg-implemented tokens
+/// @author Frigg team
 interface IFrigg {
-    /**
-     * @dev returns if primary market is opened.
-     */
-    function isPrimaryMarketActive() external view returns (bool);
-
-    /**
-     * @dev returns if the bond has expired and the issuer starts to conduct buyback.
-     */
-    function seeBondExpiryStatus() external view returns (bool);
-
-    /**
-     * @dev for primaryRouter.sol to conduct primary buy logic at issuance
-     */
-
+    /// @notice For primaryRouter.sol to conduct primary buy logic at issuance
     function mint(address account, uint256 amount) external;
 
-    /**
-     * @dev for primaryRouter.sol to conduct primary sell logic at expiry
-     */
-
+    /// @notice For primaryRouter.sol to conduct primary sell logic at expiry
     function burn(address account, uint256 amount) external;
 
-    /**
-     * @dev a getter function for dApps or third parties to fetch the terms and conditions
-     */
+    /// @notice Returns if primary market is opened.
+    function isPrimaryMarketActive() external view returns (bool);
+
+    /// @notice Returns if the bond has expired and the issuer starts to conduct buyback.
+    function seeBondExpiryStatus() external view returns (bool);
+
+    /// @notice A getter function for dApps or third parties to fetch the terms and conditions
     function termsURL() external view returns (string memory);
 }
